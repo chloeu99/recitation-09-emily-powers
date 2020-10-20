9aafedecdda0b93758fc868013cb158519086a2e
Emily Powers <epowers3@tulane.edu>
2020-10-20 21:47:55 +0000

    update

--- answers.md
+++ answers.md
@@ -1,8 +1,10 @@
 # CMPS 2200 Recitation 09
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:**Emily Powers
+**Name:**Lydia Yampolsky
+**Name:**Chloe Uhls
+**Name:**Marc Ojalvo
 
 
 Place all written answers from `recitation-09.md` here for easier grading.
@@ -10,11 +12,21 @@ Place all written answers from `recitation-09.md` here for easier grading.
 
 
 - **2)**
+W(n) = W(n-1) + W(n-2) + 1
+W(n) = 2^n
+
+
 
 - **3)**
+S(n) = S(n-1) + 1
+S(n) = O(n)
 
 - **4)**
+The last set of calls for counts, the counts list resembles a reverse fibonnacci sequence that counts down.
 
 - **6)**
+The function is called at most once for any value and the combination step is in constant time, so W(n) = O(n), S(n) = O(n)
 
 - **8)**
+The function will be called at most twice for any value.
+W(n) = O(n), S(n) = O(n)
--- main.py
+++ main.py
@@ -5,8 +5,11 @@ def fib_recursive(n, counts):
     each time fib_recursive(i, counts) is called.
     """    
     counts[n] += 1
-    ###TODO
-    pass
+    if n <= 1:
+      return n
+    else:
+      print(counts)
+      return fib_recursive(n-1, counts) + fib_recursive(n-2, counts)
     
 def test_fib_recursive():
     n = 10
@@ -16,8 +19,14 @@ def test_fib_recursive():
     print(sum(counts))
     
 def fib_top_down(n, fibs):
-    ###TODO
-    pass
+    if n <= 1:
+      return n
+    else:
+      if fibs[n] != -1:
+        return fibs[n]
+      else:
+        fibs[n] = fib_top_down(n-1,fibs) + fib_top_down(n-2,fibs)
+        return fibs[n]
 
 def test_fib_top_down():
     n = 10
@@ -26,14 +35,17 @@ def test_fib_top_down():
     print(fibs)
 
 def fib_bottom_up(n):
-    ###TODO
-    pass
+    fibs = [0,1]
+    for i in range(2,n+1):
+      next_elem = fibs[i-1] + fibs[i-2]
+      fibs.append(next_elem)
+    return fibs[-1]
 
 def test_fib_bottom_up():
     n = 10
     assert fib_bottom_up(n) == 55
 
-
+'''
 def fib_bottom_up_better(n):
     ###TODO
     pass
@@ -41,3 +53,4 @@ def fib_bottom_up_better(n):
 def test_fib_bottom_up_better():
     n = 10
     assert fib_bottom_up_better(n) == 55
+'''
